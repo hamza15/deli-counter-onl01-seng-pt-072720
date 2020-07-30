65fd1fa7a6a7cf0a08a86f726d867c828d65e673
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-30 17:09:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli.size < 1
+    puts "The line is currently empty."
+  else
+    print "The line is currently: "
+    katz_deli.each_with_index do |name, index|
+      index += 1
+      print "#{index}. #{name} "
+    end
+    print "\n"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number"
+  
+end
+  
+
+katz_deli = ["Ada", "Grace", "Kent"]
+
+line(katz_deli)

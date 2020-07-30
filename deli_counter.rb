a8aafe6750c68f64c195aab0156eeaa521f1ec64
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-30 19:04:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     new_arry = []
     counter = 0 
     while counter < katz_deli.length
-      new_arry.append("counter+1 katz_deli[counter]") do |name, index|
+      new_arry.append("#{counter+1}. #{katz_deli[counter]}") do |name, index|
       person = katz_deli.pop
       new_arry.append("#{index}")}
       counter += 1 
